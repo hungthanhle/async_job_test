eeaefdb228702137e6406d4a7564092f0a2b9e68
hung <hunglt.ee@gmail.com>
2023-05-24 08:57:44 +0700

    first test

--- app/jobs/guests_cleanup_job.rb
+++ app/jobs/guests_cleanup_job.rb
@@ -3,5 +3,12 @@ class GuestsCleanupJob < ApplicationJob
 
   def perform(*args)
     # Do something later
+    puts "báo thức sau 30 giây"
+    # rails c
+    # GuestsCleanupJob.set(wait: 30.seconds).perform_later(0) #chưa thấy return
+    # GuestsCleanupJob.perform_now(0) #return nil
+    # GuestsCleanupJob.perform_later(0) # chưa thấy return
+    # một lần thôi =)) học điện nhiều cứ tưởng lặp đi lặp lại
+    # from Async
   end
 end
